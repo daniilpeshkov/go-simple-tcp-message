d7f04b8ac50185a94687d18b87b7264a7a7ff22a
Daniil <wersfit@gmail.com>
2021-09-26 12:17:40 +0300

    added RemoveFieldIfExist() AppendField() now returns true if successful

--- message.go
+++ message.go
@@ -8,14 +8,15 @@ type Message struct {
 	fields map[byte][]byte
 }
 
-func (msg *Message) AppendField(_type byte, p []byte) {
+func (msg *Message) AppendField(_type byte, p []byte) bool {
 	if typeSupported(_type) && len(p) > 0 {
 		if _, ok := msg.fields[_type]; !ok {
 			msg.fields[_type] = make([]byte, 0, len(p))
 		}
 		msg.fields[_type] = append(msg.fields[_type], p...)
+		return true
 	}
-
+	return false
 }
 
 func NewMessage() *Message {
@@ -31,4 +32,14 @@ func (msg *Message) GetField(_type byte) ([]byte, bool) {
 		}
 	}
 	return nil, false
+}
+
+func (msg *Message) RemoveFieldIfExist(_type byte) bool {
+	if typeSupported(_type) {
+		if _, ok := msg.fields[_type]; ok {
+			delete(msg.fields, _type)
+			return true
+		}
+	}
+	return false
 }
